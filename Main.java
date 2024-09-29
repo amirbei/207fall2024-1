960fb74496652912abb51d867377b4c13bdd22b7
Amir B <amir.beisembayev@mail.utoronto.ca>
2024-09-29 19:37:59 -0400

    ikd

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Person p = new Person(first:"Paul", last:"Gries", utorid:"griespau"):
+        Person p = new Person(first:"Paul", last:"Gries", utorid:"griespau");
+        Student s = new Student("Jonathan", "Calver", "calv", "1234567890")
     }
 }
--- Person.java
+++ Person.java
@@ -5,11 +5,26 @@ public class Person {
     private final String last;
 
     private final String utorid;
-}
-    public Person{String first, String last, String utorid) {
+
+    public Person(String first, String last, String utorid) {
     this.first = first;
     this.last = last;
     this.utorid = utorid;
     }
+
+    public String getUtorid() {
+        return utorid;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String newFirst) {
+        this.first = newFirst;
+
+    }
+
+
 
 }
